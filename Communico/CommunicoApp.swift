92969fcb241914f32fc474b230b7083d303d1862
sharanyamallamula <sharanyamallamula@gmail.com>
2023-08-01 11:37:56 -0400

    Initial Commit

---
+++ Communico/CommunicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunicoApp.swift
+//  Communico
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CommunicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
