6b284fb88e493c460d42731eff1d39677b262030
sharanyamallamula <sharanyamallamula@gmail.com>
2023-08-01 15:37:31 -0400

    created the textfields for users to enter their usernames and their passwords. also created a submit button and a navigation link to transport users to the main page of the app after entering their information.

--- Communico/ContentView.swift
+++ Communico/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var enter = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            VStack {
+                Text("Welcome to")
+                Text("Communico")
+                
+                Text("Login or Sign Up now")
+                TextField("Enter Username", text: $username)
+                TextField("Enter Password", text: $password)
 
+                Button("Submit") {
+                    enter = true
+                }
+                
+                if enter == true {
+                    NavigationLink(destination:SignUpView()) {
+                        Text("Get Started")
+                    } //navlink closing
+                } // if statement closing
+            } //vstack closing
+        } //navstack closing
+    } //closing bracket
+} //closing bracket
+
+//nothing is edited under here
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Communico/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  Communico
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
